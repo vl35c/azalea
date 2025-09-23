ba4f266d5280a092dffc7b98a7e263f1192974e3
vl35c <vicklewis35@gmail.com>
2025-09-23 15:40:14 +0100

    init commit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello World")
+import sys
+import pygame
+
+from settings import *
+
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Stock Sim")
+
+    def run(self):
+        while True:
+            self.window.fill(Color.BLACK.value)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.flip()
+
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
---
+++ settings.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+class Color(Enum):
+    BLACK = "#16161d"
