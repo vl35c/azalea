c55854f9cd13e927e3a62ca035933c92bdbf3485
vl35c <vicklewis35@gmail.com>
2025-09-24 12:57:04 +0100

    buttons and functions added

--- main.py
+++ main.py
@@ -4,11 +4,41 @@ import pygame
 from settings import *
 
 
+class Button:
+    def __init__(self, x, y, width, height, color, func=lambda: None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.func = func
+
+    @property
+    def rect(self) -> pygame.Rect:
+        return pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def func(self) -> ():
+        return self.func()
+
+
 class Main:
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Stock Sim")
+
+        self.buttons = [
+            Button(100, 100, 100, 100, "red", lambda: print("I am red")),
+            Button(300, 100, 100, 100, "blue", lambda: print("I am blue"))
+        ]
+
+    def handle_mouse(self) -> None:
+        mx, my = pygame.mouse.get_pos()
+
+        for button in self.buttons:
+            if button.rect.collidepoint(mx, my):
+                button.func()
+
 
     def run(self):
         while True:
@@ -18,6 +48,11 @@ class Main:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_mouse()
+
+            for button in self.buttons:
+                pygame.draw.rect(self.window, button.color, button.rect)
 
             pygame.display.flip()
 
