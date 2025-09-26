22db9ead0527f1fe0b89dbeb7d5d249447928329
lowd3n <callum.lowden.2023@uni.strath.ac.uk>
2025-09-26 13:31:19 +0100

    Added all time low and high prices in Stock

--- stock.py
+++ stock.py
@@ -11,12 +11,18 @@ class Stock:
         self.current_high_day: float = share_value
         self.current_low_day: float = share_value
 
+        self.all_time_high_day: float = share_value
+        self.all_time_low_day: float = share_value
+
     # changes a stocks value
     def change_value(self, value: float):
         self.share_value += value
 
         if self.share_value > self.current_high_day: self.current_high_day = self.share_value
         elif self.share_value < self.current_low_day: self.current_low_day = self.share_value
+
+        if self.current_high_day > self.all_time_high_day: self.all_time_high_day = self.current_high_day
+        elif self.current_low_day > self.all_time_low_day: self.all_time_low_day = self.current_low_day
 
     def update_historic_price(self, date: int):
         self.historic_price[date] = DayShare(self.share_value, self.current_low_day, self.current_high_day)
